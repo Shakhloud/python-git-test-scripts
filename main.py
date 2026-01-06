b91071cf21550abff5114c584c2e57fa2e33c0cc
Марат Шахмоведев <llou555@yandex.ru>
2026-01-06 14:01:53 +0300

    calc v 1.0

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world!")
+from services.numsService import mathService
+
+
+def calc():
+    input_num1 = int(input("Введите первое число: "))
+    input_num2 = int(input("Введите второе число: "))
+    operation = input("Введите требуемую операцию (+, -, *, /): ")
+
+    result = None
+
+    if operation == "+":
+        result = mathService.plus_of_nums(input_num1, input_num2)
+    elif operation == "-":
+        result = mathService.min_of_nums(input_num1, input_num2)
+    elif operation == "*":
+        result = mathService.mult_of_nums(input_num1, input_num2)
+    elif operation == "/":
+        result = mathService.div_of_nums(input_num1, input_num2)
+
+    print(result, end="\n")
+
+
+if __name__ == "__main__":
+    calc()
---
+++ services/numsService.py
@@ -0,0 +1,16 @@
+class MathService:
+
+    def plus_of_nums(self, num1: int, num2: int) -> int:
+        return num1 + num2
+
+    def min_of_nums(self, num1: int, num2: int) -> int:
+        return num1 - num2
+
+    def mult_of_nums(self, num1: int, num2: int) -> int:
+        return num1 * num2
+
+    def div_of_nums(self, num1: int, num2: int) -> float:
+        return num1 / num2
+
+
+mathService = MathService()
